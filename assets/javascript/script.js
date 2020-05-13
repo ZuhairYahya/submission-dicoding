0728b9e2e82afe3eeafbedd88aa6c5d6cd3945da
Zuhair Yahya <zuhairyahya57@gmail.com>
2020-05-13 16:18:53 +0700

    mengupload hasil submission belajar di dicoding academy

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+const website = document.getElementById("website");
+const tutorial = document.getElementById("tutorial");
+const pengetahuan = document.getElementById("pengetahuan");
+
+website.innerHTML = "Website";
+tutorial.innerHTML = "Tutorial";
+pengetahuan.innerHTML = "Pengetahuan";
+
+website.style.backgroundColor = "white";
+tutorial.style.backgroundColor = "white";
+pengetahuan.style.backgroundColor = "white";
